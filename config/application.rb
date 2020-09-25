2db952354d88254d2edc8cb5555754ed41f56d6f
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-09-25 15:00:49 +0630

    creating list display function

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module Twitclone
+  class Application < Rails::Application
+
+    config.load_defaults 6.0
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
